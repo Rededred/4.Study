f6d168005f9e9c5fbebae8e36ab459bf23946c64
Andrey Zheleznyakov <79283547664@yandex.ru>
2024-04-17 20:39:25 +0300

    Немного дополнил list comprehension

--- Python — полный курс для начинающих/foreal full list comprehension.py
+++ Python — полный курс для начинающих/foreal full list comprehension.py
@@ -1,3 +1,22 @@
 [print(i**2) if not i%15 else print(i) for i in range(1, 30+1)]
 print('-'*15)
 [print(i) for i in range(1, 40+1) if not i%8]
+
+print('-'*15)
+li_co_dict = {x: x*x for x in range(1, 30)}
+print(li_co_dict)
+
+print('-'*15)
+[print(key,':', value) for key, value in {x: x*x for x in range(1, 30+1)}.items()]
+
+print('-'*15)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+transposed_matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
+[print(i) for i in transposed_matrix]
+
+print('-'*15)
+
